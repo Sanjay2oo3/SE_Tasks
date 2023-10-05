ca1d5d3f93e4f4b9b5c566cf6a566550d5d7e270
Sanjay2oo3 <itssanjaychoudhary423@gmail.com>
2023-10-05 23:54:50 +0530

    React Login Page

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Railway-Ticket-Booking-System/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Railway_Ticket_Booking_System/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle authentication logic here
+    // You'll need to send the user's credentials to the server for validation
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login to Railway Reservation System</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
